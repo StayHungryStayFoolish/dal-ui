ea5efd280e537121ccb69da8df914fb8832096d9
StayHungryStayFoolish <bonismo@hotmail.com>
2019-04-11 22:56:41 +0800

    commit

---
+++ src/main/kotlin/ink/bonismo/rest/HelloWorld.java
@@ -0,0 +1,16 @@
+package ink.bonismo.rest;
+
+/**
+ * Created by bonismo@hotmail.com on 2019/4/11 10:49 PM
+ *
+ * @Description:
+ * @Version: 1.0
+ */
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
